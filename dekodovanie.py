696098fb3322e63557c4bfac3492a6f096d33e96
bartelka <tanicka879gmail.com>
2023-01-29 18:41:32 +0100

    upravy

--- dekodovanie.py
+++ dekodovanie.py
@@ -5,24 +5,29 @@ obr = Image.open("obrazokspr.png")
 pixels = obr.load()
 
 dlzka = 8
-zoz = []
 
-for y in range(obr.size[1]):
-    for x in range(obr.size[0]):
-        pixel_blue = pixels[x,y][2]
-        blue = bin(pixel_blue)
-        zoz.append(blue[-1])
+def zisk_modrej():
+    zoz = []
+    for y in range(obr.size[1]):
+        for x in range(obr.size[0]):
+            pixel_blue = pixels[x,y][2]
+            blue = bin(pixel_blue)
+            zoz.append(blue[-1])
+    return zoz
 
-sprava = ""
-cislo = ""
+zoz = zisk_modrej()
+def desifrovacka(zoz):
+    sprava = ""
+    cislo = ""
+    for i in zoz:
+        x = ""
+        cislo += i
+        if len(cislo) == dlzka:
+            x = chr(int(cislo,2))
+            sprava += x
+            cislo = ""
+        if "#" in sprava:
+            break
+    return sprava
 
-for i in zoz:
-    x = ""
-    cislo += i
-    if len(cislo) == dlzka:
-        x = chr(int(cislo,2))
-        sprava += x
-        cislo = ""
-    if "#" in sprava:
-        break
-print(sprava)
+print(desifrovacka(zoz))
